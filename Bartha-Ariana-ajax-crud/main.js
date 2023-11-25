d4afe4bbd75f72fbf956738c042d6ee08d3193b6
Bartha Ariana Janka <ariabartha@gmail.com>
2023-11-25 11:43:39 +0100

    adatok között a bool value cserélve text-re, új adatbázis generálva, minden átírva e szerint

--- Bartha-Ariana-ajax-crud/main.js
+++ Bartha-Ariana-ajax-crud/main.js
@@ -1,8 +1,38 @@
-const api_url = "https://retoolapi.dev/J0NGgL/productOrder";
+const api_url = "https://retoolapi.dev/T4Mjdu/productOrder";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const ordersForm = document.getElementById("ordersForm");
+  ordersForm.addEventListener("submit", addOrder);
   listOrders();
 });
+
+
+async function addOrder(event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const product = document.getElementById('product').value;
+  const date = document.getElementById('dateOfOrder').value;
+  const status = document.getElementById('status').value;
+  const order = {
+    name: name,
+    email: email,
+    product: product,
+    date: date,
+    status: status
+  };
+  console.log(order);
+  console.log(JSON.stringify(order));
+
+  const response = await fetch(api_url, {
+    method: "POST",
+    body: JSON.stringify(order),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+}
 
 function listOrders() {
   const ordersTable = document.getElementById("ordersTable");
@@ -12,22 +42,22 @@ function listOrders() {
         const tableRow = document.createElement("tr");
         const idTableData = document.createElement("td");
         const nameTableData = document.createElement("td");
-        const sentTableData = document.createElement("td");
         const emailTableData = document.createElement("td");
         const productTableData = document.createElement("td");
         const dateOfOrderTableData = document.createElement("td");
+        const statusTableData = document.createElement("td");
         idTableData.textContent = orders.id;
-        nameTableData.textContent = orders.Name;
-        sentTableData.textContent = orders.Sent;
-        emailTableData.textContent = orders.Email;
-        productTableData.textContent = orders.Product;
-        dateOfOrderTableData.textContent = orders.Date_of_order;
+        nameTableData.textContent = orders.name;
+        emailTableData.textContent = orders.email;
+        productTableData.textContent = orders.product;
+        dateOfOrderTableData.textContent = orders.date_of_order;
+        statusTableData.textContent = orders.delivery_status;
         tableRow.appendChild(idTableData);
         tableRow.appendChild(nameTableData);
-        tableRow.appendChild(sentTableData);
         tableRow.appendChild(emailTableData);
         tableRow.appendChild(productTableData);
         tableRow.appendChild(dateOfOrderTableData);
+        tableRow.appendChild(statusTableData);
         ordersTable.appendChild(tableRow);
       });
     });
