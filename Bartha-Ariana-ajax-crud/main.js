8415568bb170c1a4d2ae1f9a94003e4016709e4b
Bartha Ariana Janka <ariabartha@gmail.com>
2023-11-25 09:03:01 +0100

    táblázatsorok kiírására új függvény írva

--- Bartha-Ariana-ajax-crud/main.js
+++ Bartha-Ariana-ajax-crud/main.js
@@ -1,20 +1,34 @@
 const api_url = "https://retoolapi.dev/J0NGgL/productOrder";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const ordersTable = document.getElementById("ordersTable");
-  fetch(api_url).then(httpResponse => httpResponse.json()).then(responseBody => {
-    let htmlTable = "";
-    responseBody.forEach(orders => {
-      const tableRow = `<tr>
-      <td>${orders.id}</td>
-      <td>${orders.Name}</td>
-      <td>${orders.Sent}</td>
-      <td>${orders.Email}</td>
-      <td>${orders.Product}</td>
-      <td>${orders.Date_of_order}</td>
-      </tr>`;
-      htmlTable += tableRow;
-    });
-    ordersTable.innerHTML = htmlTable;
-  });
+  listOrders();
 });
+
+function listOrders() {
+  const ordersTable = document.getElementById("ordersTable");
+  fetch(api_url).then(httpResponse => httpResponse.json())
+    .then(responseBody => {
+      responseBody.forEach(orders => {
+        const tableRow = document.createElement("tr");
+        const idTableData = document.createElement("td");
+        const nameTableData = document.createElement("td");
+        const sentTableData = document.createElement("td");
+        const emailTableData = document.createElement("td");
+        const productTableData = document.createElement("td");
+        const dateOfOrderTableData = document.createElement("td");
+        idTableData.textContent = orders.id;
+        nameTableData.textContent = orders.Name;
+        sentTableData.textContent = orders.Sent;
+        emailTableData.textContent = orders.Email;
+        productTableData.textContent = orders.Product;
+        dateOfOrderTableData.textContent = orders.Date_of_order;
+        tableRow.appendChild(idTableData);
+        tableRow.appendChild(nameTableData);
+        tableRow.appendChild(sentTableData);
+        tableRow.appendChild(emailTableData);
+        tableRow.appendChild(productTableData);
+        tableRow.appendChild(dateOfOrderTableData);
+        ordersTable.appendChild(tableRow);
+      });
+    });
+}
